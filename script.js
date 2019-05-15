de3552359ba582ce7500f6d140b28bdc7ca84d57
snarkshow <laura.hw.chuang@gmail.com>
2019-05-14 21:01:24 -0400

    added scss

---
+++ script.js
@@ -0,0 +1,30 @@
+const flashcards = {
+    deutsch: [
+        {
+            germanWord: "kaffee",
+            englishWord: "coffee"
+        },
+        {
+            germanWord: "milch",
+            englishWord: "milk"
+        },
+        {
+            germanWord: "schwarz",
+            englishWord: "black"
+        },
+        {
+            germanWord: "heiß",
+            englishWord: "hot"
+        }
+        {
+            germanWord: "schokolade",
+            englishWord: "chocolate"
+        },
+        {
+            germanWord: "bitte",
+            englishWord: "please"
+        },
+    ]
+}
+
+
